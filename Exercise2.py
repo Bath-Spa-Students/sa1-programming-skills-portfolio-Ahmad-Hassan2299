53c145ee59decead66171e8795e71cbbeb3e4597
Ahmad-Hassan2299 <ahmadhassan2299@icloud.com>
2025-03-06 16:49:42 +0400

    Biography

---
+++ Exercise2.py
@@ -0,0 +1,25 @@
+# This program stores and prints your name, hometown, and age using a dictionary
+# Function to get user input and validate age
+def get_user_input():
+     # Get user input for name, hometown, and age
+    name = input("Enter your name: ")
+    hometown = input("Enter your hometown: ")
+    # Validate age input
+    while True:
+        age = input("Enter your age: ")
+        if age.isdigit():
+            age = int(age)
+            break
+        else:
+            print("Please enter a valid age (intiger value).")
+    return name, hometown, age
+# Geting user input
+name, hometown, age = get_user_input()
+# Saveing the information in a dictionary
+biography = {
+    "Name": name,
+    "Hometown": hometown,
+    "Age": age
+}
+# Printing the values on separate lines using a single print() statement
+print(f"Name: {biography['Name']}\nHometown: {biography['Hometown']}\nAge: {biography['Age']}")

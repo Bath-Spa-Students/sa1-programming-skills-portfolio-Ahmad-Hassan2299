00ed592dce4fb0add43fb4f55d00277ba11f95ef
Ahmad-Hassan2299 <ahmadhassan2299@icloud.com>
2025-03-22 11:04:00 +0400

    Update Exercise0.py

--- Exercise0.py
+++ Exercise0.py
@@ -1,8 +1,8 @@
 #assigning value to a variable 1
-word1 = "Coding"
+word1 =  "Coding"
 #assigning value to a variable 2
-word2 = "is"
+word2 =  "is"
 #assigning value to a variable 3
-word3 = "Cool"
+word3 =  "Cool"
 # use string concatenation to combine the variables and print the phrase
 print(word1 + " " + word2 + " " + word3)
